7e7f4e568bd7ac9aa2c13d0fe1405485b7ca667e
huutamcbt <huutamcbt@gmail.com>
2023-01-28 23:52:01 +0700

    Linters and formatter

---
+++ lua/config/null-ls.lua
@@ -0,0 +1,14 @@
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    debug = false,
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.cpplint,
+        null_ls.builtins.completion.spell,
+        -- null_ls.builtins.formatting.astyle,
+        -- null_ls.builtins.formatting.lua_format,
+    },
+})
